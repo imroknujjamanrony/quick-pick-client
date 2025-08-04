e074d77212a5bda4641382470b65c328c1c7a8dd
KM Adnan Absar <mdadnanjilan1234@gmail.com>
2025-08-04 21:25:35 +0600

    first-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css'
-import ProductPage from './components/DetailsPage'
+import ProductPage from './Components/DetailsPage'
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-     <div className='w-[80%] mx-auto'>
-      <ProductPage/>
-     </div>
-        
-=======
       <h1>hello</h1>
->>>>>>> fd072b3ae8f14da3a40bab0ed62890f680f759bc
     </>
   )
 }
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout/MainLayout";
 import Home from "../Pages/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import ProductPage from "../Components/DetailsPage";
+import Register from "../Pages/Register";
+import Login from "../Pages/Login";
 
 
 export const router = createBrowserRouter([
@@ -13,7 +16,16 @@ export const router = createBrowserRouter([
         {
             path: "/",
             element: <Home></Home>
-        }
+        },
+        {
+          path:'/register',
+          element:<Register></Register>
+        },
+        {
+          path:'/login',
+          element: <Login></Login>
+        },
+        
       ]
     },
   ]);
