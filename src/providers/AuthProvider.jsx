a02cb5345cb9fcce4d2ce71ae500a7eef5c50ea5
Ssiyam0123 <ssiyam563@gmail.com>
2025-08-18 13:05:48 +0600

    push

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,47 +1,50 @@
 import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase_init";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const [user , setUser] = useState(null)
-  const [loading , setLoading] = useState(true)
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  const createUser = (email , password)=> {
-    setLoading(true)
-   return createUserWithEmailAndPassword(auth , email , password)
-  }
+  const signInUser = (email, password) => {
+    setLoading(true);
 
-  const signInUser = (email , password)=> {
-        setLoading(true)
-
-return signInWithEmailAndPassword(auth , email , password)
-  }
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const logout = () => {
-    setLoading(true)
-    return signOut(auth)
-  }
+    setLoading(true);
+    return signOut(auth);
+  };
 
-useEffect(() => {
-  const unSubscribe = onAuthStateChanged(auth , currentUser => {
-    console.log('current user',currentUser);
-    setUser(currentUser)
-    setLoading(false)
-    
-  })
-  return () =>{
-    unSubscribe();
-  }
-}, [])
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("current user", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
   const userInfo = {
-user , 
-loading,
-createUser,
-signInUser,
-logout
+    user,
+    loading,
+    createUser,
+    signInUser,
+    logout,
   };
 
   return (
